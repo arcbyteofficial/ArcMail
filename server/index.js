658b687ea834b621d3c03d97edfc37e75ea93ec6
Arcbyte Co <arcbyte.co@gmail.com>
2026-03-28 04:42:51 +0530

    ArcMail

--- server/index.js
+++ server/index.js
@@ -128,7 +128,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options('/*', cors(corsOptions));
 
 app.use((req, res, next) => {
   const origin = req.headers.origin;
